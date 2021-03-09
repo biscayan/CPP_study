c8fcaf52b3927dea87f0849998dcb62a6a832081
biscayan <skgudwn34@gmail.com>
2021-03-09 19:52:09 +0900

    객체지향 프로그래밍과 클래스

---
+++ Section8/section8_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// object는 속성과 기능을 가지고 있음
+// data만 넣을 때는 struct를 사용하고 기능까지 넣을 때는 class를 사용하는 것이 일반적임
+class Friend
+{
+	//access classifier (public, private, protected)
+public:
+	string m_name;
+	int m_age;
+	double m_height;
+	double m_weight;
+
+	void print()
+	{
+		cout << m_name << " " << m_age << " " << m_height << " " << m_weight << endl;
+	}
+};
+
+int main()
+{
+	// Instanciation
+	// jj, kh는 Friend class의 instance임
+	Friend jj{"jackjack", 8, 100, 45.8};
+	Friend kh{"Kimich", 28, 183.2, 85};
+
+	return 0;
+}
